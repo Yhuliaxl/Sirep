a80fc20db6e5c90b5a912bf68701034277225c50
Yanira3108 <yanirajimenez312004@gmail.com>
2025-05-02 11:47:18 -0500

    creacion de la tabla cargo con su api, views, routes, url, serializers y modelos.

--- Sirep 2.0/apps/empresa/cargo/models.py
+++ Sirep 2.0/apps/empresa/cargo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class cargo(models.Model):
+    id_sena = models.AutoField(
+        primary_key=True,
+        verbose_name="Identificador único del cargo"
+    )
+    nombre = models.CharField(
+        max_length=45,
+        null=True,
+        blank=True,
+        verbose_name="Nombre del cargo"
+    )
+    num_factura = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Número consecutivo de facturación"
+    )
+
+    class Meta:
+        db_table = 'cargo'
+        verbose_name = 'cargo'
+        verbose_name_plural = 'cargos'
+
+    def __str__(self):
+        return self.nombre or f'cargo {self.id_sena}'
