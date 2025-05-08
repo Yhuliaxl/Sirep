93bc0a9352c59311e1e982bc2ea6f89d0173183f
yhuliaxl <yhuliaxl@gmail.com>
2025-05-08 17:59:19 -0500

    modificando la foreign key fk_persona en el modelo Movimiento y ajustando el método __str__ para mostrar el nombre de la persona asociada

--- Sirep 2.0/apps/movimiento/movimientos/models.py
+++ Sirep 2.0/apps/movimiento/movimientos/models.py
@@ -1,5 +1,6 @@
 # apps/inventario/movimientos/models.py
 from django.db import models
+from apps.empresa.personas.models import persona
 
 class Movimiento(models.Model):
     ESTADO_CHOICES = [
@@ -16,16 +17,14 @@ class Movimiento(models.Model):
     id_movimiento = models.AutoField(primary_key=True)
     estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, blank=True, null=True)
     fecha = models.DateField(blank=True, null=True)
-    # fk_persona = models.ForeignKey('personas.Persona', on_delete=models.CASCADE)
-    fk_persona = models.BigIntegerField()  # Temporalmente como BigIntegerField
+    fk_persona = models.ForeignKey(persona,on_delete=models.CASCADE,verbose_name="Persona asociada",related_name="movimientos" )
     tipo = models.CharField(max_length=20, choices=TIPO_CHOICES, blank=True, null=True)
     num_factura = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return f"Movimiento {self.id_movimiento} - Persona {self.fk_persona}"
+        persona_nombre = self.fk_persona.nombre if self.fk_persona else "Sin persona"
+        return f"Movimiento {self.id_movimiento} - Persona {persona_nombre}"
 
     class Meta:
         verbose_name = "Movimiento"
         verbose_name_plural = "Movimientos"
-
-# Create your models here.
