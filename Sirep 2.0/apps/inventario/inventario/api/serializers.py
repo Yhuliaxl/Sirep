49c115b58a2aa3f50a8ba8860fabe73216f7e722
yhuliaxl <yhuliaxl@gmail.com>
2025-05-08 15:58:20 -0500

    actualizando el modelo Inventario para usar foreign keys en lugar de campos temporales y ajustando el serializer correspondiente

--- Sirep 2.0/apps/inventario/inventario/api/serializers.py
+++ Sirep 2.0/apps/inventario/inventario/api/serializers.py
@@ -5,4 +5,4 @@ from apps.inventario.inventario.models import Inventario
 class InventarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventario
-        fields = ['id_inventario', 'id_bodega', 'id_producto', 'cantidad', 'fecha_actual']
+        fields = ['id_inventario', 'fk_id_punto_vent', 'fk_codigo_pdto', 'cantidad', 'fecha_actual']
