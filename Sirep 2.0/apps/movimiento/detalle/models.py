d02863a51bc407f5ca858414810b385a446a41af
yhuliaxl <yhuliaxl@gmail.com>
2025-05-08 17:53:48 -0500

    agregando las foreign keys fk_id_movimiento y fk_id_inventario en el modelo Detalle y ajustando el método __str__

--- Sirep 2.0/apps/movimiento/detalle/models.py
+++ Sirep 2.0/apps/movimiento/detalle/models.py
@@ -1,4 +1,7 @@
+# apps/inventario/detalle/models.py
 from django.db import models
+from apps.movimiento.movimientos.models import Movimiento
+from apps.inventario.inventario.models import Inventario
 
 class Detalle(models.Model):
     ESTADO_CHOICES = [
@@ -18,16 +21,15 @@ class Detalle(models.Model):
     estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, blank=True, null=True)
     entregado = models.CharField(max_length=20, choices=ENTREGADO_CHOICES)
     persona = models.BigIntegerField(blank=True, null=True)
-    # fk_id_movimiento = models.ForeignKey('movimiento.Movimiento', on_delete=models.CASCADE)
-    # fk_id_inventario = models.ForeignKey('inventario.Inventario', on_delete=models.CASCADE)
-    fk_id_movimiento = models.IntegerField()  # Temporalmente como IntegerField
-    fk_id_inventario = models.IntegerField()  # Temporalmente como IntegerField
+    fk_id_movimiento = models.ForeignKey(Movimiento,on_delete=models.CASCADE,verbose_name="Movimiento asociado",related_name="detalles")
+    fk_id_inventario = models.ForeignKey(Inventario,on_delete=models.CASCADE,verbose_name="Inventario asociado",related_name="detalles")
 
     def __str__(self):
-        return f"Detalle {self.id_detalle} - Movimiento {self.fk_id_movimiento}"
+        movimiento_id = self.fk_id_movimiento.id_movimiento if self.fk_id_movimiento else "Sin movimiento"
+        return f"Detalle {self.id_detalle} - Movimiento {movimiento_id}"
 
     class Meta:
         verbose_name = "Detalle"
         verbose_name_plural = "Detalles"
         
-        # Create your models here.
+    
