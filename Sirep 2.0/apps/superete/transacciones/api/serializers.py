398a329a7fb7d19eb9cffe6192d44ae76f6d41c3
yhuliaxl <yhuliaxl@gmail.com>
2025-05-16 00:45:43 -0500

    Actualizar serializer de transacciones para incluir campos relevantes y eliminar los innecesarios.

--- Sirep 2.0/apps/superete/transacciones/api/serializers.py
+++ Sirep 2.0/apps/superete/transacciones/api/serializers.py
@@ -5,6 +5,10 @@ class TransaccionesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaccion
         fields = [
-            'id', 'caja_id', 'tipo', 'monto', 'descripcion',
-            'fecha', 'creado', 'actualizado'
+            'tipo',
+            'producto_id',
+            'cantidad',
+            'precio_unitario',
+            'fecha',
+            'persona_id',
         ]
